93d84df1ae8ae266f19ba4426e64fa96ce183229
pms512 <pms512@hanmail.net>
2024-01-29 15:04:48 +0900

    add linked list example code

---
+++ linkedList.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void helloWorld();
+
+typedef struct _USERDATA {
+	char name[20];
+	int age;
+	char phone[14];
+	struct _USERDATA *pNext;
+} USERDATA;
+
+int main()
+{
+	USERDATA *pUser;
+	USERDATA person[3] = {
+		{"ABCD", 20, "010-1234-5678", NULL},
+		{"EFGH", 25, "010-2345-6789", NULL},
+		{"IJKL", 30, "010-3456-7890", NULL}
+	};
+
+	person[0].pNext = &person[1];
+	person[1].pNext = &person[2];
+	person[2].pNext = NULL;
+
+	pUser = &person[0];
+
+
+	while( pUser != NULL )
+	{
+		printf("[%p] %s %d %s [%p]\n", pUser, pUser->name, pUser->age, pUser->phone, pUser->pNext);
+		pUser = pUser->pNext;
+	}	
+	
+	helloWorld();
+
+	return 0;
+}
+
